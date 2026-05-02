ad3e9f0b29a3abed314abd07e8e84551b41260e8
Muhammad Zain Ur Rehman <32788650+zain505@users.noreply.github.com>
2026-05-02 12:12:36 +0500

    feat: update Vite configuration to include asset handling and base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ["**/*.glb"],
+  base: '/husnain_web/',
 })
